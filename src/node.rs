afd088d41f384ebfabfa237a43b21c8f37e8c50b
Brian Picciano <me@mediocregopher.com>
2025-10-14 17:08:25 +0200

    Wait for node to finish syncing regardless of reported blocks
    The highest/current blocks might match even when syncing, e.g. if some
    middle stage was recently added and is still running. If any non-false
    response is returned from eth_syncing we should just assume the node is
    syncing.

--- src/node.rs
+++ src/node.rs
@@ -348,9 +348,7 @@ impl NodeManager {
                 match provider.syncing().await {
                     Ok(sync_result) => {
                         match sync_result {
-                            SyncStatus::Info(sync_info)
-                                if sync_info.current_block != sync_info.highest_block =>
-                            {
+                            SyncStatus::Info(sync_info) => {
                                 debug!("Node is still syncing {sync_info:?}, waiting...");
                             }
                             _ => {
